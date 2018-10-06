99a1d17db756627e69e1257cb23385be3afe6db7
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-06 11:36:13 -0400

    set up custom tab structure

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { FeedPage } from "../feed/feed";
+import { UserSearchPage } from "../user-search/user-search"
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab1Root = FeedPage;
+  tab2Root = UserSearchPage;
   tab3Root = ContactPage;
 
   constructor() {
